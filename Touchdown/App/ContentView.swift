28843a45bdb02103bf4f9db1e625757c81b7481b
Roman Tolmachev <tolmachevroman@gmail.com>
2024-03-12 08:35:02 -0300

    add NavigationBarView

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -13,8 +13,23 @@ struct ContentView: View {
     // MARK: - BODY
     
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(.white)
+                    .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 5)
+                
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            }
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        }
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 
---
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -0,0 +1,54 @@
+//
+//  NavigationBarView.swift
+//  Touchdown
+//
+//  Created by Roman Tolmachev on 12.03.2024.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var isAnimated: Bool = false
+    
+    var body: some View {
+        HStack {
+            Button(action: {}, label: {
+                Image(systemName: "magnifyingglass")
+                    .font(.title)
+                    .foregroundColor(.black)
+            })
+            
+            Spacer()
+            
+            LogoView()
+                .opacity(isAnimated ? 1 : 0)
+                .offset(x: 0, y: isAnimated ? 0 : -25)
+                .onAppear(perform: {
+                    withAnimation(.easeOut(duration: 0.5)) {
+                        isAnimated.toggle()
+                    }
+                })
+            
+            Spacer()
+            
+            Button(action: {}, label: {
+                ZStack {
+                    Image(systemName: "cart")
+                        .font(.title)
+                    .foregroundColor(.black)
+                    
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 14, height: 14, alignment: .center)
+                        .offset(x: 13, y: -10)
+                }
+            })
+        }
+    }
+}
+
+#Preview {
+    NavigationBarView()
+}
