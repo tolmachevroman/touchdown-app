95070511eeec649354c5890df9e7ef0549af7fdc
Roman Tolmachev <tolmachevroman@gmail.com>
2024-03-12 07:10:58 -0300

    initial project setup

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Roman Tolmachev on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
