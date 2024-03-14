76d3d7410675c385d0a3ccebe3269e9881e0ef99
Roman Tolmachev <tolmachevroman@gmail.com>
2024-03-13 21:08:38 -0300

    add RatingsSizesDetailView

--- Touchdown/App/ProductDetailView.swift
+++ Touchdown/App/ProductDetailView.swift
@@ -23,18 +23,36 @@ struct ProductDetailView: View {
             // DETAIL TOP PART
             TopPartDetailView()
                 .padding(.horizontal)
+                .zIndex(1)
             
             // DETAIL BOTTOM PART
+            VStack(alignment: .center, spacing: 0, content: {
+                // RATINGS + SIZES
+                RatingsSizesDetailView()
+                    .padding(.top, -20)
+                    .padding(.bottom, 10)
+                            
+                // DESCRIPTION
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    Text(sampleProduct.description)
+                        .font(.system(.body, design: .rounded))
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.leading)
+                })
+                            
+                // QUANTITY + FAVORITE
+                            
+                // ADD TO CART
+                Spacer()
+            })
+            .padding(.horizontal)
+            .background(
+                Color.white.clipShape(CustomShape())
+                .padding(.top, -105)
+            )
             
-            // RATINGS + SIZES
-            
-            // DESCRIPTION
-            
-            // QUANTITY + FAVORITE
-            
-            // ADD TO CART
-            Spacer()
         })
+        .zIndex(0)
         .ignoresSafeArea(.all, edges: .all)
         .background(
             Color(red: sampleProduct.red, green: sampleProduct.green, blue: sampleProduct.blue)
